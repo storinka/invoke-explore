fcb3803657676521c8015d502508191ee810165c
Oleksii Lozoviahin <Oleksih@gmail.com>
2022-03-11 16:39:32 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { viteSingleFile } from "vite-plugin-singlefile"
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), viteSingleFile()],
-    base: '/invoke-explore/',
+    base: '/',
 	build: {
     cssCodeSplit: false,
     assetsInlineLimit: 100000000,
