7824bba58d9cf48610868fdc40c85743c60cb37e
Oleksii <oleksii@aquaminer.me>
2022-03-11 16:49:02 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { viteSingleFile } from "vite-plugin-singlefile"
+import {viteSingleFile} from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), viteSingleFile()],
     base: '/',
-	build: {
-    cssCodeSplit: false,
-    assetsInlineLimit: 100000000,
-    rollupOptions: {
-      output: {
-        manualChunks: () => "everything.js",
-      },
+    build: {
+        cssCodeSplit: false,
+        assetsInlineLimit: 100000000,
+        rollupOptions: {
+            output: {
+                manualChunks: () => "everything.js",
+            },
+        },
+
     },
-	
-},
 
 })
